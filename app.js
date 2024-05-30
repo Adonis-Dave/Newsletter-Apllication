54b095e6166e5b2186ad2156c82a0fe06795c0ae
David Denis <daviddenis656@gmail.com>
2024-05-30 15:35:40 +0300

    Modified the back home button on the success page to redirect back to the landing page

--- app.js
+++ app.js
@@ -63,6 +63,10 @@ app.post("/failure", function (req, res) {
     res.redirect("/");
 })
 
+app.post("/success", function (req, res) {
+    res.redirect("/");
+})
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
